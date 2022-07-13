58cbb7b91941912a372286ffe8bab5ecf0529438
Jayanth0912 <109193468+Jayanth0912@users.noreply.github.com>
2022-07-13 23:42:35 +0530

    cm1
    cm1

--- Python/Arrays.py
+++ Python/Arrays.py
@@ -132,5 +132,19 @@
 # print(text)
 
 
+# text=input("Enter the String: ")
+#
+# vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+# newtext = ""
+# textlen = len(text)
+#
+# for i in range(textlen):
+#     if text[i] in vowels:
+#         newtext = newtext + text[i]
+#
+# print("\nString after removing Vowels: ")
+# text = newtext
+# print(text)
+
 
 
